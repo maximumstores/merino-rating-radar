6b867244d70b7d0ec23d1603435b09b726cbba4a
Merino tech <maximumstores53@gmail.com>
2026-09-03 09:22:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -750,4 +750,4 @@ else:
                 margin=dict(l=20, r=20, t=40, b=20),
             )
 
-            st.plotly_chart(fig_single, use_container_width=True)
+            st.plotly_chart(fig_single, use_container_width=True) 
